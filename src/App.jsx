896b47f95808df87a1602edfb7b84a4b415a4e8c
VishalPachpor <Vishalpatil080502@gmail.com>
2023-10-12 19:33:11 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 
-
+import Card from './components/Card'
 import './App.css'
 
 function App() {
-
+  let details = "Read more about the mackbook series here";
+  let name = "Display Name";
+  let nextText = "Browse more";
+  let details2 = "Details regarding the product"
 
   return (
     <>
-     <h1>Tailwind And Props in React </h1>
-    
+     <h1 className='bg-green-400 text-black'>Tailwind And Props in React </h1>
+      <Card about="This is Mac" next="Click me" desc={details}/> 
+      <Card about={name} next={nextText} desc={details2}/> 
     </>
   )
 }
